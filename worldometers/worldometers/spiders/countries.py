fabfbdd954abf0e1bd4f4fab6a80500ea90d23a1
clfo2014@gmail.com <clfo2014@gmail.com>
2023-02-09 20:10:58 -0500

    section 4 complete

--- worldometers/worldometers/spiders/countries.py
+++ worldometers/worldometers/spiders/countries.py
@@ -1,4 +1,5 @@
 import scrapy
+import logging
 
 
 class CountriesSpider(scrapy.Spider):
@@ -12,7 +13,21 @@ class CountriesSpider(scrapy.Spider):
             name = country.xpath(".//text()").get()
             link = country.xpath(".//@href").get()
         
+            yield response.follow(url = link, callback = self.parse_country, meta = {"country_name": name})
+            
+    def parse_country(self, response):
+        #logging.info(response.url)
+        name = response.request.meta["country_name"]
+        rows = response.xpath("(//table[@class='table table-striped table-bordered table-hover table-condensed table-list'])[1]/tbody/tr")
+        for row in rows:
+            year = row.xpath(".//td[1]/text()").get()
+            population = row.xpath(".//td[2]/strong/text()").get()
             yield {
-                "country_name": name,
-                "country_link": link
+                "country_name": name, 
+                "year": year,
+                "population": population
             }
+            
+
+# NOTES
+# - Sync data between 2 parse methods, we need a 'request meta'
