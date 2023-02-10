85a8c27d0e2165b65c06952ec9cd9eda2c8f7c9a
clfo2014@gmail.com <clfo2014@gmail.com>
2023-02-09 22:58:44 -0500

    section 6 building the spider complete

---
+++ tinydeal/tinydeal/spiders/special_offers.py
@@ -0,0 +1,19 @@
+import scrapy
+
+
+class SpecialOffersSpider(scrapy.Spider):
+    name = "special_offers"
+    allowed_domains = ["www.web.archive.org"]
+    start_urls = ["https://web.archive.org/web/20190225123327/https://www.tinydeal.com/specials.html"]
+
+    def parse(self, response):
+        for product in response.xpath("//ul[@class='productlisting-ul']/div/li"):
+            yield {
+                "title":            product.xpath(".//a[@class='p_box_title']/text()").get(),
+                "url":              response.urljoin(product.xpath(".//a[@class='p_box_title']/@href").get()),
+                "discounted_price": product.xpath(".//span[@class='productSpecialPrice fl']/text()").get(),
+                "original_price":   product.xpath(".//span[@class='normalprice fl']/text()").get()                
+                
+                
+            }
+
