7caf95f15cfeb4c6a43f1c0c3ddae9ea6073f7b3
Lucas Okwudishu <62886078+LucasO21@users.noreply.github.com>
2023-03-14 22:41:11 -0400

    add techcrunch spider

--- techcrunch/techcrunch/spiders/articles.py
+++ techcrunch/techcrunch/spiders/articles.py
@@ -2,9 +2,22 @@ import scrapy
 
 
 class ArticlesSpider(scrapy.Spider):
+    
     name = "articles"
-    allowed_domains = ["techcrunch.com"]
-    start_urls = ["https://techcrunch.com/category/apps/"]
+    allowed_domains = ["www.techcrunch.com"]
+    start_urls = ["https://techcrunch.com/category/apps"]
 
     def parse(self, response):
-        pass
+        for post in response.xpath("////div[@class='post-block post-block--image post-block--unread']"):
+            
+            title  = post.xpath(".//a[@class='post-block__title__link']/text()").get().strip()
+            date   = post.xpath(".//div[@class='river-byline']/time/text()").get().strip()
+            author = post.xpath(".//span[@class='river-byline__authors']/a/text()").get().strip()
+            link   = post.xpath(".//a[@class='post-block__title__link']/@href").get().strip()
+            
+            yield {
+                "title":  title,
+                "date":   date,
+                "author": author,
+                "link":   link
+            }
