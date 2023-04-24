2d66af955d3568cbb1fc7cd6944ac7abe2bd4ffa
Lucas Okwudishu <62886078+LucasO21@users.noreply.github.com>
2023-04-24 07:29:36 -0400

    fix parse_items in vehicles2

--- projects/gtabase/gtabase/spiders/vehicles2.py
+++ projects/gtabase/gtabase/spiders/vehicles2.py
@@ -57,24 +57,50 @@ class Vehicles2Spider(scrapy.Spider):
                 posting_card = soup.find_all('div', class_=lambda x: x and x.startswith('item_'))
 
                 # - Extract Vehicle Card
-                for post in posting_card:
-                    link = post.find('a', class_='product-item-link')['href']
+                links = [card.find('a', class_='product-item-link')['href'] for card in soup.find_all('div', class_ = lambda x: x and x.startswith('item_'))]
+                links = ["https://www.gtabase.com/" + i for i in links]
+                links = links[:3]
                     
-                    # - Yield Link
-                    yield {"link": link}
+                # - Yield Link
+                # yield {"link": link} # - Testing
+                for link in links:
+                    yield SeleniumRequest(url = link, callback = self.parse_items)                
 
                 # - Check If Next Page Xpath is Disabled. If Disabled Then Break. If Not Disabled, Click On Next Page Xpath
                 if self.driver.find_elements("xpath", "//li[contains(@class, 'pages-item-next') and contains(@class, 'disabled')]"):
                     break
                 else:
-                    next_button = self.driver.find_element("xpath", "//a[@class='page action next']")
-                    next_button.click()
-                    WDW(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'product') and contains(@class, 'item') and contains(@class, 'ln-element')]")))              
+                    try:
+                        next_button = self.driver.find_element("xpath", "//a[@class='page action next']")
+                        next_button.click()
+                        WDW(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'product') and contains(@class, 'item') and contains(@class, 'ln-element')]")))     
+                    except:
+                        break         
 
             # Exception Handling
             except Exception as e:
                 self.logger.error(f"Error: {e}")
                 # Handle the exception appropriately
+    
+    # Parse Items Method
+    def parse_items(self, response):
+        
+        # Open URL
+        self.driver.get(response.url)
+        
+        # Store Response
+        resp = Selector(text = self.driver.page_source)
+        
+        # Extract Items
+        for vehicle_info in resp.xpath("//div[@class='article-content']"):
+            name = vehicle_info.xpath(".//h2[5]/text()").get(default = "NA").strip()
+            manufacturer = vehicle_info.xpath(".//dl/dd//span//a[contains(@title, 'Vehicle Class')]/text()").get(default = "NA").strip()
+        
+            yield {
+                  "name": name,
+                  "manufacturer": manufacturer
+            }   
+        
 
     # Close Spider
     def spider_closed(self, reason):
